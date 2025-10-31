e0bbe5c62b312c99c2365f77460ced792472e545
LEI-122706 <csfoe@iscte-iul.pt>
2025-10-31 17:36:27 +0000

    atualizacao frigate test

--- src/test/java/iscteiul/ista/battleship/FrigateTest.java
+++ src/test/java/iscteiul/ista/battleship/FrigateTest.java
@@ -6,10 +6,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FrigateTest {
 
-    private final Frigate f = new Frigate(Compass.NORTH, new Position(5,5));
+    private final Frigate f1 = new Frigate(Compass.NORTH, new Position(5,5));
+    private final Frigate f2 = new Frigate(Compass.SOUTH, new Position(5,5));
+    private final Frigate f3 = new Frigate(Compass.EAST, new Position(5,5));
+    private final Frigate f4 = new Frigate(Compass.WEST, new Position(5,5));
 
     @Test
     void getSize() {
-        assertEquals(4, f.getSize());
+        assertEquals(4, f1.getSize());
+        assertEquals(4, f2.getSize());
+        assertEquals(4, f3.getSize());
+        assertEquals(4, f4.getSize());
     }
 }
